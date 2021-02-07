a2f8668c7d02c5aa245d50d08d10c793beaa320c
Debian <bitnami@ip-172-26-10-167.ec2.internal>
2021-02-07 17:50:01 +0000

    Use wp plug in scafold change plug in name

---
+++ qr-generator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     QR Code Generator
+ * Plugin URI:      PLUGIN  URL
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Yamil L Cruz
+ * Author URI:      https://www.linkedin.com/in/yamilcruz/
+ * Text Domain:     sample-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         QR Code Generator
+ */
+
+// Your code starts here.
