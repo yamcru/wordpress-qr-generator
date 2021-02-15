575039b38d627e82632253b235c646a77584afd7
Yamil Cruz <yamil.cruz3@gmail.com>
2021-02-14 20:51:29 -0400

    Remove twenty twentyone theme dependencies

--- src/templates/qr_code.php
+++ src/templates/qr_code.php
@@ -38,16 +38,13 @@ get_header();
                                 <?php the_title( sprintf( '<h2 class="entry-title default-max-width"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
                             <?php endif; ?>
 
-                            <?php twenty_twenty_one_post_thumbnail(); ?>
                         </header><!-- .entry-header -->
 
                         <div class="entry-content">
                             <img src="https://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=<?php echo urlencode(esc_url( get_permalink() )."?re=1"); ?>"></img>
                         </div><!-- .entry-content -->
 
-                        <footer class="entry-footer default-max-width">
-                            <?php twenty_twenty_one_entry_meta_footer(); ?>
-                        </footer><!-- .entry-footer -->
+   
                     </article><!-- #post-<?php the_ID(); ?> -->
                 <?php
 
